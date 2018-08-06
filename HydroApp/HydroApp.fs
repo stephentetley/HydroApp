b713930c56cbbb39a75cc3d0348a8aa8f4c8c22c
stephentetley <stephen.tetley@gmail.com>
2018-08-06 22:57:42 +0100

    Extending the model to have option<decimal> type for Span and Spill.

--- HydroApp/HydroApp.fs
+++ HydroApp/HydroApp.fs
@@ -12,37 +12,57 @@ module App =
 
 
     type Hydro = 
-        | Hydro200
+        | Hydroranger
         | HydroPlus
+        | Hydro200
     
     /// The model from which the view is generated
     type Model =
         { HydroType: Hydro
-          Span: string
-          Spill: string }
+          SerialNumber: string
+          Span: option<decimal>
+          Spill: option<decimal> }
+
+    let optionText (fn:'a -> string) (source:option<'a>) : string = 
+        match source with
+        | None -> ""
+        | Some a -> fn a
 
     /// The messages dispatched by the view
     type Msg =
         | HydroChanged of Hydro
+        | SerialNumberChanged of string
         | SpanChanged of string
         | SpillChanged of string
 
     /// Returns the initial state
     let init() : Model = 
-        { HydroType=HydroPlus
-          Span = ""
-          Spill = "" }
+        { HydroType = HydroPlus
+          SerialNumber = ""
+          Span = None
+          Spill = None }
 
     /// The funtion to update the view
     let update (msg:Msg) (model:Model) : Model =
         match msg with
         | HydroChanged t -> { model with HydroType = t }
-        | SpanChanged s -> { model with Span = s}
-        | SpillChanged s -> { model with Spill = s}
+        | SerialNumberChanged s -> { model with SerialNumber = s}
+        | SpanChanged s -> 
+            try
+                let d = decimal s in { model with Span = Some d}
+            with
+            | _ -> model
+        | SpillChanged s -> 
+            try
+                let d = decimal s in { model with Spill = Some d}
+            with
+            | _ -> model
     
     let pickerItems = 
-        [| ("Hydroranger Plus", Hydro.HydroPlus)
-        ;  ("Hydroranger 200", Hydro.Hydro200) |]
+        [| ("Hydroranger",          Hydro.Hydroranger)
+         ; ("Hydroranger Plus",     Hydro.HydroPlus)
+         ; ("Hydroranger 200",      Hydro.Hydro200) 
+        |]
 
 
     /// The view function giving updated content for the page
@@ -58,11 +78,16 @@ module App =
                                       itemsSource=(Array.map fst pickerItems), 
                                       horizontalOptions=LayoutOptions.Start, 
                                       selectedIndexChanged= fun (i, item) -> dispatch (HydroChanged (snd pickerItems.[i])))
+                ; View.Label(text="Serial Number:")
+                ; View.Entry(text= model.SerialNumber, 
+                             textChanged = fun (args:TextChangedEventArgs) -> dispatch (SerialNumberChanged args.NewTextValue))
                 ; View.Label(text="Span:")
-                ; View.Entry(text= model.Span, 
+                ; View.Entry(text = optionText (fun a -> a.ToString()) model.Span,
+                             keyboard = Keyboard.Numeric,
                              textChanged = fun (args:TextChangedEventArgs) -> dispatch (SpanChanged args.NewTextValue))
                 ; View.Label(text="Spill:")
-                ; View.Entry(text= model.Spill, 
+                ; View.Entry(text = optionText (fun a -> a.ToString()) model.Spill, 
+                             keyboard = Keyboard.Numeric,
                              textChanged = fun (args:TextChangedEventArgs) -> dispatch (SpillChanged args.NewTextValue))
                 ]))
 
